a6ca2ba76265cf076fbe84330eb9e855176edb4f
jeferson rocha <jeferson.rocha8@gmail.com>
2019-11-21 11:38:07 -0300

    projeto criado

---
+++ android/app/src/main/java/com/jr/uber_clone/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jr.uber_clone;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
