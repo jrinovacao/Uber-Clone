0ec2375a9c99f1828febe4505c9f49560762e5ca
jeferson rocha <jeferson.rocha8@gmail.com>
2019-11-23 10:08:55 -0300

    add thema padrao

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:uber_clone/telas/Home.dart';
 
+final ThemeData temaPadrao = ThemeData(
+  primaryColor: Color(0xff37474f),
+  accentColor: Color(0xff546e7a),
+);
+
 void main() => runApp(MaterialApp(
   title: "Uber Clone",
   home: Home(),
